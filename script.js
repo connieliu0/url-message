3baea7f248dfbd56baa490530b0e59be37016696
Connie Liu <connieliu@Connies-Laptop.local>
2024-08-27 09:44:05 -0700

    mm

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const dialog = document.querySelector("dialog");
+    const showButton = document.querySelector("dialog + button");
+    const closeButton = document.querySelector("dialog button");
+
+    // "Show the dialog" button opens the dialog modally
+    showButton.addEventListener("click", () => {
+    dialog.showModal();
+    });
+
+    // "Close" button closes the dialog
+    closeButton.addEventListener("click", () => {
+    dialog.close();
+    });
+
     const selectContainer = document.getElementById('select-container');
     const imageContainers = document.querySelectorAll('.image-container .selected-image');
     let doneUnblur = false;
